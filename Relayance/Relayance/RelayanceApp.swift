6af4cd96f20a5e4559463585b4343362599c950b
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-07-08 12:20:28 +0200

    Initialisation du projet

---
+++ Relayance/Relayance/RelayanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelayanceApp.swift
+//  Relayance
+//
+//  Created by Amandine Cousin on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RelayanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
